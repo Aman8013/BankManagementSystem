cbd88105e23461413b932ed03ad9a4170a825a17
Aman Omkar <amanomkar823001@gmail.com>
2024-03-31 21:31:16 +0530

    BankManagementSystem Done

--- BankManagementSystem.java
+++ BankManagementSystem.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import static java.lang.constant.ConstantDescs.NULL;
+
 public class BankManagementSystem {
 
    static  final  String url = "jdbc:mysql://localhost:3306/bank";
@@ -25,7 +27,75 @@ public class BankManagementSystem {
             Account account=new Account(connection,scanner);
             AccountManager accountManager=new AccountManager(connection,scanner);
             User user =new User(connection,scanner);
-
+            String email;
+            long accountNumber;
+            while (true){
+                System.out.println("Welcome to BANK MANAGEMENT SYSTEM");
+                System.out.println();
+                System.out.println("1. Register");
+                System.out.println("2. Login");
+                System.out.println("3. Exit");
+                System.out.println("Enter your Choice");
+                int choice=scanner.nextInt();
+                switch (choice){
+                    case 1:
+                           user.register();
+                           break;
+                    case 2:
+                           email=user.login();
+                           if(email!=NULL){
+                               System.out.println();
+                               System.out.println("User is logged in");
+                               if(!account.account_exists(email)){
+                                   System.out.println();
+                                   System.out.println("1. Open Bank Account");
+                                   System.out.println("2. Exit");
+                                   if(scanner.nextInt()==1){
+                                       accountNumber=account.open_account(email);
+                                       System.out.println("Account Opened Successfully");
+                                       System.out.println("Your account Number is"+" "+accountNumber);
+                                   }else{
+                                       break;
+                                   }
+                               }
+                           }
+                           long account_Number=account.get_accountNumber(email);
+                           int choice2=scanner.nextInt();
+                           while(choice2!=5){
+                               System.out.println();
+                               System.out.println("1. Debit Money");
+                               System.out.println("2. Credit Money");
+                               System.out.println("3. Transfer Money");
+                               System.out.println("4. Get Balance");
+                               System.out.println("5. Log Out");
+                               choice2= scanner.nextInt();
+                               switch (choice2){
+                                   case 1:
+                                          accountManager.debit_Money(account_Number);
+                                          break;
+                                   case 2:
+                                          accountManager.credit_Money(account_Number);
+                                          break;
+                                   case 3:
+                                          accountManager.transfer_Money(account_Number);
+                                          break;
+                                   case 4:
+                                          accountManager.getBalance(account_Number);
+                                          break;
+                                   case 5:
+                                          break;
+                                   default:
+                                          System.out.println("Invalid Choice");
+                                          break;
+                               }
+                           }
+                    case 3:
+                          System.out.println("Thank You For Using BANK MANAGEMENT SYSTEM");
+                          return;
+                    default:
+                          System.out.println("Invalid choice");
+                }
+            }
 
         }
         catch(Exception e){
